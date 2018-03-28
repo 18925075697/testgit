f1662dee6d83c3121ff7ccb4717a8e1854309386
chenjingkun <791068331@qq.com>
2018-03-28 10:10:04 +0800

    提交

--- app/src/main/java/com/cjk/gittest/MainActivity.java
+++ app/src/main/java/com/cjk/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //a
+        //b这是新的分支测试
     }
 }
